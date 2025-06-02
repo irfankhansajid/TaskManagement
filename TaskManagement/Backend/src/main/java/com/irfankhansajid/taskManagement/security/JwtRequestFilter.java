74e637b614efcaa3908846637a28bbd630d74fbd
irfankhansajid <irfankhansajid@gmail.com>
2025-06-02 22:56:57 +0600

    chore: Prepare for branch restructure

--- TaskManagement/Backend/src/main/java/com/irfankhansajid/taskManagement/security/JwtRequestFilter.java
+++ TaskManagement/Backend/src/main/java/com/irfankhansajid/taskManagement/security/JwtRequestFilter.java
@@ -27,7 +27,7 @@ public class JwtRequestFilter extends OncePerRequestFilter{
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
+    protected void doFilterInternal( HttpServletRequest request,
       HttpServletResponse response, 
       FilterChain filterChain)
             throws ServletException, IOException {
